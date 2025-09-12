26e48c45805ee10c4bd0fcdd75828fb14882f9b3
Ruben Lee Landrø Johnsen <rubenlee.johnsen@skatteetaten.no>
2025-09-12 14:10:01 +0200

    render to triangle There's not really a benefit to this for such a small project, but it's so easy to do that I might as well.

--- src/window.zig
+++ src/window.zig
@@ -16,14 +16,12 @@ const ScreenQuad = struct {
 
     pub fn init() ScreenQuad {
         const vertices = [_]f32{
-            -1.0, -1.0, 0.0, 0.0,
-            1.0,  1.0,  1.0, 1.0,
-            1.0,  -1.0, 1.0, 0.0,
-            -1.0, 1.0,  0.0, 1.0,
+            -3.0, -1.0, -1.0, 0.0,
+            1.0,  -1.0, 1.0,  0.0,
+            1.0,  3.0,  1.0,  2.0,
         };
         const indices = [_]u32{
             0, 1, 2,
-            3, 1, 0,
         };
 
         var vao: c_uint = undefined;
